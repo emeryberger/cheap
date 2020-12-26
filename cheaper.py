a5b301054599ea346d1d3e8c9a4da98aaf3b65a0
emery.berger@gmail.com <emery.berger@gmail.com>
2020-12-26 15:14:07 -0500

    Refactored; changed to use depth from top of stack frame.

--- cheaper.py
+++ cheaper.py
@@ -1,6 +1,5 @@
 """cheaper.py: identifies where to apply custom allocators"""
 
-# import json
 import orjson
 import math
 import os
@@ -8,14 +7,25 @@ import argparse
 import sys
 import subprocess
 from collections import defaultdict
+from textwrap import dedent
 
-parser = argparse.ArgumentParser("cheaper")
-parser.add_argument("--progname", help="path to executable")
+usage = dedent(
+    """Cheaper: finds where you should use a custom heap.
+    """
+)
+parser = argparse.ArgumentParser(
+    prog="cheaper",
+    description=usage,
+    formatter_class=argparse.RawTextHelpFormatter,
+    allow_abbrev=False,
+)
+parser.add_argument("--progname", help="path to executable", required=True)
 parser.add_argument(
     "--threshold-mallocs", help="threshold allocations to report", default=100
 )
 parser.add_argument("--threshold-score", help="threshold reporting score", default=0.8)
-parser.add_argument("--skip", help="number of stack frames to skip", default=1)
+# parser.add_argument("--skip", help="number of stack frames to skip", default=1)
+parser.add_argument("--depth", help="total number of stack frames to use (from top)", default=5)
 
 args = parser.parse_args()
 
@@ -23,54 +33,60 @@ if not args.progname:
     parser.print_help()
     sys.exit(-1)
 
-depth = int(args.skip)
+# skip = int(args.skip)
+depth = int(args.depth)
 progname = args.progname
 
 with open("cheaper.out", "r") as f:
     file_contents = f.read()
 
 x = orjson.loads(file_contents)
-# x = json.loads(file_contents)
 
-trace = x["trace"]
 
-stack_series = defaultdict(list)
-
-stack_info = {}
-
-# Convert each stack frame into a name and line number
-for i in trace:
-    for stkaddr in i["stack"][depth:]:
-        if stkaddr not in stack_info:
-            result = subprocess.run(
-                ["addr2line", hex(stkaddr), "-e", progname], stdout=subprocess.PIPE
-            )
-            stack_info[stkaddr] = result.stdout.decode("utf-8").strip()
-    # print(stack_info[stkaddr])
-
-# Separate each trace by its complete stack signature.
-for i in trace:
-    stk = [stack_info[k] for k in i["stack"][depth:]]
-    stack_series[str(stk)].append(i)
-
-# Iterate through each call site, in descending order by number of allocations
-for k in sorted(stack_series, key=len, reverse=True):
-    sizes = set()
-    size_histogram = defaultdict(int)
-    if len(stack_series[k]) < int(args.threshold_mallocs):
+def analyze(allocs, stackstr):
+    """Analyze a trace of allocations and frees."""
+    if len(allocs) < int(args.threshold_mallocs):
         # Ignore call sites with too few mallocs
-        break # since sorted, the others all have fewer mallocs
-    # Compute total memory footprint (excluding frees)
-    # We use this to compute a "region score" later.
-    total_footprint = 0
-    for i in stack_series[k]:
+        return
+    # The set of sizes of allocated objects.
+    sizes = set()
+    # A histogram of the # of objects allocated of each size.
+    size_histogram = defaultdict(int)
+    actual_footprint = 0  # mallocs - frees
+    peak_footprint = 0  # max actual_footprint
+    nofree_footprint = 0  # sum(mallocs)
+    # set of all thread ids used for malloc/free
+    tids = set()
+    # set of all (currently) allocated objects from this site
+    mallocs = set()
+    for i in allocs:
         sizes.add(i["size"])
         size_histogram[i["size"]] += 1
+        tids.add(i["tid"])
         if i["action"] == "M":
-            total_footprint += i["size"]
+            # Compute actual footprint (taking into account mallocs and frees).
+            actual_footprint += i["size"]
+            if actual_footprint > peak_footprint:
+                peak_footprint = actual_footprint
+            # Compute total 'no-free' memory footprint (excluding frees) This
+            # is how much memory would be consumed if we didn't free anything
+            # until the end (as with regions/arenas). We use this to compute a
+            # "region score" later.
+            nofree_footprint += i["size"]
+            # Record the malloc so we can check it when freed.
+            mallocs.add(i["address"])
+        elif i["action"] == "F":
+            if i["address"] in mallocs:
+                # Only reclaim memory that we have already allocated
+                # (others are frees to other call sites).
+                actual_footprint -= i["size"]
+                mallocs.remove(i["address"])
+            else:
+                pass
+                # print(mallocs)
+                # print(str(i["address"]) + " not found")
     # Compute entropy of sizes
-    total = len(stack_series[k])
-    # print(size_histogram)
+    total = len(allocs)
     normalized_entropy = -sum(
         [
             float(size_histogram[c])
@@ -79,29 +95,16 @@ for k in sorted(stack_series, key=len, reverse=True):
             for c in size_histogram
         ]
     ) / len(size_histogram)
-    # print(normalized_entropy)
-    # print(len(size_histogram))
-    # Compute actual footprint
-    actual_footprint = 0
-    # set of all thread ids used for malloc/free
-    tids = set()
-    # set of all (currently) allocated objects from this site
-    mallocs = set()
-    for i in stack_series[k]:
-        tids.add(i["tid"])
-        if i["action"] == "M":
-            actual_footprint += i["size"]
-            mallocs.add(i["address"])
-        elif i["action"] == "F":
-            if i["address"] in mallocs:
-                actual_footprint -= i["size"]
-                mallocs.remove(i["address"])
     # Compute region_score (0 is worst, 1 is best - for region replacement).
     region_score = 0
-    if total_footprint != 0:
-        region_score = actual_footprint / total_footprint
+    if nofree_footprint != 0:
+        region_score = peak_footprint / nofree_footprint
     if region_score >= float(args.threshold_score):
-        print(k, "allocations = ", len(stack_series[k]))
+        stk = eval(stackstr)
+        print("=====")
+        print(stk[0])
+        print("-----")
+        print("allocations = ", len(allocs))
         print("candidate for region allocation")
         print("* region score = " + str(region_score), "(1 is best)")
         if len(tids) == 1:
@@ -113,6 +116,32 @@ for k in sorted(stack_series, key=len, reverse=True):
         else:
             print(str(len(sizes)) + " different sizes = ", size_list)
             print("size entropy = ", normalized_entropy)
-        print("total footprint = " + str(total_footprint))
-        print("actual footprint = " + str(actual_footprint))
-        print("----")
+        print("peak footprint = " + str(peak_footprint))
+        print("'no free' footprint = " + str(nofree_footprint))
+
+def process_trace(trace):
+    stack_series = defaultdict(list)
+    stack_info = {}
+
+    # Convert each stack frame into a name and line number
+    for i in trace:
+        for stkaddr in i["stack"][-depth:]:
+            if stkaddr not in stack_info:
+                result = subprocess.run(
+                    ["addr2line", hex(stkaddr), "-i", "-f", "-C", "-e", progname],
+                    stdout=subprocess.PIPE,
+                )
+                stack_info[stkaddr] = result.stdout.decode("utf-8").strip()
+
+    # Separate each trace by its complete stack signature.
+    for i in trace:
+        stk = [stack_info[k] for k in i["stack"][-depth:]] # [skip:depth+skip]]
+        stack_series[str(stk)].append(i)
+
+    # Iterate through each call site.
+    for k in stack_series:
+        analyze(stack_series[k], k)
+
+
+trace = x["trace"]
+process_trace(trace)
