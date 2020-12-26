0609447adf3b20660a2475b8b511113d2a5074b8
emery.berger@gmail.com <emery.berger@gmail.com>
2020-12-26 16:45:39 -0500

    Added volatiles to prevent elimination by optimizer.

--- test/regional.cpp
+++ test/regional.cpp
@@ -1,12 +1,13 @@
-// run with 'unbuffer'!
-
 #include <iostream>
 
 void doIt() {
-  const int num = 1000;
-  char * arr[num];
-  for (int i = 0; i < num; i++) {
+  const int num = 2000;
+  volatile char * volatile arr[num];
+  for (int i = 0; i < num / 2; i++) {
     arr[i] = new char[16];
+  }
+  for (int i = num / 2; i < num; i++) {
+    arr[i] = new char[16]; // i / 100 + 1];
   }
   for (int i = 0; i < num; i++) {
     delete [] arr[i];
