2e55b922ea6558c7c55c2a52cf0ab4732454f248
Emery Berger <emery.berger@gmail.com>
2021-01-05 14:53:09 -0500

    Reformatted.

--- cheapen.h
+++ cheapen.h
@@ -3,24 +3,22 @@
 #include <stdlib.h>
 
 extern "C" {
-  void region_begin(void * buf, size_t sz);
-  void region_end();
+void region_begin(void *buf, size_t sz);
+void region_end();
 }
 
 #if defined(__cplusplus)
-template <size_t RegionSize>
-class cheapen_region {
+template <size_t RegionSize> class cheapen_region {
 public:
-  cheapen_region()
-    : _buf (new char[RegionSize])
-    {
-      region_begin(_buf, RegionSize);
-    }
+  cheapen_region() : _buf(new char[RegionSize]) {
+    region_begin(_buf, RegionSize);
+  }
   ~cheapen_region() {
     region_end();
-    delete [] _buf;
+    delete[] _buf;
   }
+
 private:
-  char * _buf;
+  char *_buf;
 };
 #endif
