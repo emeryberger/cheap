bae44b6b8dbbf5e4c7d5ee79ef908f05673f5a54
emery.berger@gmail.com <emery.berger@gmail.com>
2021-01-08 10:58:48 -0500

    Added flags.

--- cheapen.h
+++ cheapen.h
@@ -3,18 +3,19 @@
 #include <stdlib.h>
 
 extern "C" {
-  void region_begin(void *buf, size_t sz, bool allAligned = false, bool allSameSize = false);
+  void region_begin(void *buf, size_t sz, bool allAligned = false, bool allNonZero = false, bool sizeTaken = true);
   void region_end();
 }
 
 #if defined(__cplusplus)
 template <size_t RegionSize,
 	  bool AllAligned = false,
-	  bool AllSameSize = false>
+	  bool AllNonZero = false,
+	  bool SizeTaken = true>
 class cheapen_region {
 public:
   cheapen_region() : _buf(new char[RegionSize]) {
-    region_begin(_buf, RegionSize, AllAligned, AllSameSize);
+    region_begin(_buf, RegionSize, AllAligned, AllNonZero, SizeTaken);
   }
   ~cheapen_region() {
     region_end();
