7bf18be5b58d88e754a3e8587c1a06451458b2ef
emery.berger@gmail.com <emery.berger@gmail.com>
2021-01-08 11:00:04 -0500

    Moved info into class, updated flags.

--- libcheapen.cpp
+++ libcheapen.cpp
@@ -36,24 +36,33 @@ CustomHeapType &getTheCustomHeap() {
   return thang;
 }
 
-static thread_local bool in_region = false;
-static thread_local char *region_buffer = nullptr;
-static thread_local size_t region_size_remaining = 0;
-static thread_local bool all_aligned = false;  //  if true, no need to align sizes
-static thread_local bool all_same_size = false;//  if true, can cache a size to return from malloc_usable_size
+class cheap_info {
+public:
+  bool in_region { false };
+  char *region_buffer { nullptr };
+  size_t region_size_remaining { 0 };
+  bool all_aligned { false };  //  if true, no need to align sizes
+  bool all_nonzero { false };  //  if true, no zero size requests
+  bool size_taken { true }; // if true, need metadata for size
+};
 
-extern "C" ATTRIBUTE_EXPORT void region_begin(void *buf, size_t sz, bool allAligned = false, bool allSameSize = false) {
-  region_buffer = reinterpret_cast<char *>(buf);
-  region_size_remaining = sz;
-  in_region = true;
-  all_aligned = allAligned;
-  all_same_size = allSameSize;
+static thread_local cheap_info info;
+
+extern "C" ATTRIBUTE_EXPORT void region_begin(void *buf, size_t sz, bool allAligned = false, bool allNonZero = false, bool sizeTaken = true) {
+  auto& ci = info;
+  ci.region_buffer = reinterpret_cast<char *>(buf);
+  ci.region_size_remaining = sz;
+  ci.in_region = true;
+  ci.all_aligned = allAligned;
+  ci.all_nonzero = allNonZero;
+  // FIXME handle sizes
 }
 
 extern "C" ATTRIBUTE_EXPORT void region_end() {
-  in_region = false;
-  region_buffer = nullptr;
-  region_size_remaining = 0;
+  auto& ci = info;
+  ci.in_region = false;
+  ci.region_buffer = nullptr;
+  ci.region_size_remaining = 0;
 }
 
 extern "C" ATTRIBUTE_EXPORT size_t xxmalloc_usable_size(void *ptr) {
@@ -62,27 +71,30 @@ extern "C" ATTRIBUTE_EXPORT size_t xxmalloc_usable_size(void *ptr) {
 }
 
 extern "C" ATTRIBUTE_EXPORT void *xxmalloc(size_t sz) {
-  if (in_region) {
+  auto& ci = info;
+  if (ci.in_region) {
     // Enforce default alignment.
-    if (!all_aligned) {
-      if (sz < alignof(max_align_t)) {
-	sz = alignof(max_align_t);
+    if (!ci.all_aligned) {
+      if (!ci.all_nonzero) {
+	if (sz < alignof(max_align_t)) {
+	  sz = alignof(max_align_t);
+	}
       }
       sz = (sz + alignof(max_align_t) - 1) & ~(alignof(max_align_t) - 1);
     }
-    if (region_size_remaining < sz) {
+    if (ci.region_size_remaining < sz) {
       return nullptr;
     }
-    auto oldbuf = region_buffer;
-    region_buffer += sz;
-    region_size_remaining -= sz;
+    auto oldbuf = ci.region_buffer;
+    ci.region_buffer += sz;
+    ci.region_size_remaining -= sz;
     return oldbuf;
   }
   return getTheCustomHeap().malloc(sz);
 }
 
 extern "C" ATTRIBUTE_EXPORT void xxfree(void *ptr) {
-  if (!in_region) {
+  if (!info.in_region) {
     getTheCustomHeap().free(ptr);
   }
 }
@@ -92,10 +104,11 @@ extern "C" ATTRIBUTE_EXPORT void xxfree_sized(void *ptr, size_t sz) {
 }
 
 extern "C" ATTRIBUTE_EXPORT void *xxmemalign(size_t alignment, size_t sz) {
-  if (in_region) {
+  auto& ci = info;
+  if (ci.in_region) {
     // Round up the region pointer to the required alignment.
-    const auto bufptr = reinterpret_cast<uintptr_t>(region_buffer);
-    region_buffer =
+    auto bufptr = reinterpret_cast<uintptr_t>(ci.region_buffer);
+    ci.region_buffer =
         reinterpret_cast<char *>((bufptr + alignment - 1) & ~(alignment - 1));
     return xxmalloc(sz);
   }
