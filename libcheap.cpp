8e4148299d6d3b769a4a1a73a648d940020e3ff8
emery.berger@gmail.com <emery.berger@gmail.com>
2021-01-11 12:04:53 -0500

    Force inlining, renamed variable to .

--- libcheap.cpp
+++ libcheap.cpp
@@ -44,7 +44,7 @@ CustomHeapType &getTheCustomHeap() {
 
 class cheap_info {
 public:
-  bool in_region{false};
+  bool in_cheap{false};
   char *region_buffer{nullptr};
   size_t region_size_remaining{0};
   bool all_aligned{false}; //  if true, no need to align sizes
@@ -76,7 +76,7 @@ extern "C" ATTRIBUTE_EXPORT void region_begin(void *buf, size_t sz,
   auto &ci = info;
   ci.region_buffer = reinterpret_cast<char *>(buf);
   ci.region_size_remaining = sz;
-  ci.in_region = true;
+  ci.in_cheap = true;
   ci.all_aligned = allAligned;
   ci.all_nonzero = allNonZero;
   ci.size_taken = sizeTaken;
@@ -84,24 +84,24 @@ extern "C" ATTRIBUTE_EXPORT void region_begin(void *buf, size_t sz,
 
 extern "C" ATTRIBUTE_EXPORT void region_end() {
   auto &ci = info;
-  ci.in_region = false;
+  ci.in_cheap = false;
   ci.region_buffer = nullptr;
   ci.region_size_remaining = 0;
 }
 
-extern "C" ATTRIBUTE_EXPORT inline size_t xxmalloc_usable_size(void *ptr) {
+extern "C" size_t __attribute__((always_inline)) xxmalloc_usable_size(void *ptr) {
   auto &ci = info;
-  if (ci.in_region) {
+  if (ci.in_cheap) {
     assert(ci.size_taken);
     return ((cheap_header *)ptr - 1)->object_size;
   }
   return getTheCustomHeap().getSize(ptr);
 }
 
-extern "C" ATTRIBUTE_EXPORT inline void *xxmalloc(size_t req_sz) {
+extern "C" void * __attribute__((always_inline)) xxmalloc(size_t req_sz) {
   auto &ci = info;
   size_t sz = req_sz;
-  if (ci.in_region) {
+  if (ci.in_cheap) {
     if (!ci.all_aligned) {
       // Enforce default alignment.
       if (!ci.all_nonzero) {
@@ -133,19 +133,19 @@ extern "C" ATTRIBUTE_EXPORT inline void *xxmalloc(size_t req_sz) {
   return getTheCustomHeap().malloc(sz);
 }
 
-extern "C" ATTRIBUTE_EXPORT inline void xxfree(void *ptr) {
-  if (!info.in_region) {
+extern "C" void __attribute__((always_inline)) xxfree(void *ptr) {
+  if (!info.in_cheap) {
     getTheCustomHeap().free(ptr);
   }
 }
 
-extern "C" ATTRIBUTE_EXPORT inline void xxfree_sized(void *ptr, size_t) {
+extern "C" void __attribute__((always_inline)) xxfree_sized(void *ptr, size_t) {
   xxfree(ptr);
 }
 
-extern "C" ATTRIBUTE_EXPORT inline void *xxmemalign(size_t alignment, size_t sz) {
+extern "C" void * __attribute__((always_inline)) xxmemalign(size_t alignment, size_t sz) {
   auto &ci = info;
-  if (ci.in_region) {
+  if (ci.in_cheap) {
     // Round up the region pointer to the required alignment.
     auto bufptr = reinterpret_cast<uintptr_t>(ci.region_buffer);
     ci.region_buffer =
@@ -155,9 +155,9 @@ extern "C" ATTRIBUTE_EXPORT inline void *xxmemalign(size_t alignment, size_t sz)
   return getTheCustomHeap().memalign(alignment, sz);
 }
 
-extern "C" ATTRIBUTE_EXPORT inline void xxmalloc_lock() { getTheCustomHeap().lock(); }
+extern "C" void __attribute__((always_inline)) xxmalloc_lock() { getTheCustomHeap().lock(); }
 
-extern "C" ATTRIBUTE_EXPORT inline void xxmalloc_unlock() {
+extern "C" void __attribute__((always_inline)) xxmalloc_unlock() {
   getTheCustomHeap().unlock();
 }
 
