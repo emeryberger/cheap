a9915682802754ead521da017ce687f5bf7aa7a9
emery.berger@gmail.com <emery.berger@gmail.com>
2021-01-11 18:46:19 -0500

    Revised API to eliminate template argument.

--- examples/json/testjson.cpp
+++ examples/json/testjson.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <string_view>
 
 #if !defined(CHEAPEN)
 #define CHEAPEN 0
@@ -13,11 +14,15 @@
 
 #include "json.hpp"
 
-void parseMe()
+void parseMe(std::string_view& view)
 {
+  //  std::ifstream i("gsoc-2018.json");
+#if 0
   nlohmann::json j;
-  std::ifstream i("gsoc-2018.json");
   i >> j;
+#else
+  auto volatile r = nlohmann::json::parse(view.begin(), view.end()); // data, sz);
+#endif
 }
 
 void outputMe(nlohmann::json& j)
@@ -31,12 +36,20 @@ void outputMe(nlohmann::json& j)
 int
 main()
 {
-  for (auto i = 0; i < 100; i++)
+  std::ifstream i("citm_catalog.json");
+  std::ostringstream sstr;
+  sstr << i.rdbuf();
+  //  mallopt(M_MMAP_THRESHOLD, 10487560 + 32);
+  auto str = sstr.str();
+  char * data = str.data();
+  size_t sz = str.size();
+  auto view = std::string_view(data, sz);
+  for (auto i = 0; i < 1000; i++)
   {
 #if CHEAPEN
-    cheap::cheap<5436432> reg(cheap::NONZERO | cheap::SINGLE_THREADED);
+    cheap::cheap reg(cheap::NONZERO | cheap::SINGLE_THREADED);
 #endif
-    parseMe();
+    parseMe(view);
   }
   return 0;
 }
