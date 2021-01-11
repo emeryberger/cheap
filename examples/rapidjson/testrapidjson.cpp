bdf6a69ce33f3aee0163955db23c2c587bf801f1
emery.berger@gmail.com <emery.berger@gmail.com>
2021-01-10 19:34:13 -0500

    Use basic allocator.

--- examples/rapidjson/testrapidjson.cpp
+++ examples/rapidjson/testrapidjson.cpp
@@ -24,9 +24,9 @@ void parseMe(const char * s, size_t sz)
     cheap::cheap<10485760> reg(cheap::ALIGNED | cheap::NONZERO | cheap::SINGLE_THREADED | cheap::SIZE_TAKEN); //  | cheap::SIZE_TAKEN
 #endif
     using namespace rapidjson;
-    //    CrtAllocator alloc;
-    //    GenericDocument<UTF8<>, CrtAllocator> d(&alloc);
-    Document d;
+        CrtAllocator alloc;
+        GenericDocument<UTF8<>, CrtAllocator> d(&alloc);
+	//Document d;
     //    GenericDocument<UTF8<>> d;
     d.Parse(s, sz);
   }
@@ -34,11 +34,12 @@ void parseMe(const char * s, size_t sz)
 
 int main()
 {
-  std::ifstream i("gsoc-2018.json");
+  //   std::ifstream i("gsoc-2018.json");
+  std::ifstream i("citm_catalog.json");
   std::ostringstream sstr;
   sstr << i.rdbuf();
   //  mallopt(M_MMAP_THRESHOLD, 10487560 + 32);
-  for (auto i = 0; i < 100; i++)
+  for (auto i = 0; i < 1000; i++)
   {
     parseMe(sstr.str().data(), sstr.str().size());
   }
