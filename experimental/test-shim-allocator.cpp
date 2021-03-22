cb289c11f08c72e7a04e2211f81bb3ca9d8ab85f
emery.berger@gmail.com <emery.berger@gmail.com>
2021-03-22 09:44:02 -0400

    Added shuffling.

--- experimental/test-shim-allocator.cpp
+++ experimental/test-shim-allocator.cpp
@@ -1,31 +1,74 @@
+#include <algorithm>
+#include <iostream>
+#include <random>
 #include <string.h>
 
 #include "shim_allocator.hpp"
 
-#define USE_MALLOC 1
+#ifndef USE_MALLOC
+#define USE_MALLOC 0
+#endif
+
+#ifndef USE_SHUFFLE
+#define USE_SHUFFLE 1
+#endif
 
 #if USE_MALLOC
-#warning "Using malloc"
 #define MALLOC(x) malloc(x)
 #define FREE(x) free(x)
 #else
-#warning "Using the shim"
 #define MALLOC(x) alloc.allocate(x)
 #define FREE(x) alloc.deallocate(x)
 #endif
 
+
 int main()
 {
-  constexpr size_t sz = 8;
-  ShimAllocator alloc;
+  constexpr size_t objectSize = 64;
   constexpr int its = 1000000;
+  constexpr int reps = 10000;
+
+#if USE_MALLOC
+  std::cout << "Malloc "; 
+#else
+  std::cout << "Shim   ";
+#endif
+  std::cout << " objectSize=" << objectSize << ", reps = " << reps << ", each rep: iterations=" << its;
+
+#if USE_SHUFFLE
+  std::cout << " [shuffled]";
+#endif
+  std::cout << std::endl;
+  
+  ShimAllocator alloc;
   auto ptrs = new void *[its];
+
+
+  std::random_device rdev;
+  std::mt19937 rng(rdev());
+  
+  // Shuffle memory.
+  for (auto i = 0; i < its; i++) {
+    ptrs[i] = MALLOC(objectSize);
+  }
+#if USE_SHUFFLE
+  std::shuffle(ptrs, ptrs + its, rng);
+#endif
+  for (auto i = 0; i < its; i++) {
+    FREE(ptrs[i]);
+  }
+  
+  // Now repeatedly allocate and free.
   volatile void * ptr = nullptr;
-  for (auto its = 0; its < 10000; its++) {
+  volatile char target[objectSize];
+  for (auto its = 0; its < reps; its++) {
     for (auto i = 0; i < its; i++) {
-      ptr = MALLOC(sz);
+      ptr = MALLOC(objectSize);
       ptrs[i] = (void *) ptr;
-      memset((void *) ptr, 0, sz);
+      memset((void *) ptr, 0, objectSize);
+    }
+    for (auto i = 0; i < its; i++) {
+      memcpy((void *) &target, (void *) ptrs[i], objectSize);
     }
 #if USE_MALLOC
     for (auto i = 0; i < its; i++) {
