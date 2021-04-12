a13657d5e22cd0926ce7ffeec1bffeca94f08c95
emery.berger@gmail.com <emery.berger@gmail.com>
2021-04-12 09:49:49 -0400

    Updated.

--- experimental/test-shim-buffermanager.cpp
+++ experimental/test-shim-buffermanager.cpp
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <algorithm>
 #include <iostream>
 #include <random>
@@ -5,28 +6,71 @@
 #include <bslstl_string.h>
 #include <bdlma_bufferedsequentialallocator.h>
 #include <string>
+#include <cstring>
 #include <memory>
 
 #include "shim_buffermanager.hpp"
 
 #include "fragmenter.hpp"
 
+#if !defined(USE_BUFFER) && !defined(USE_SHIM)
+#define USE_BUFFER 1
+#define USE_SHIM 0
+#endif
+
+#if USE_BUFFER + USE_SHIM != 1
+#error "One must be chosen"
+#endif
+
+#if !defined(SHUFFLE)
+#define SHUFFLE false
+//#define SHUFFLE true
+#endif
+
+// no shim, no shuffle: 0.07MB (mimalloc), 2.543s
+// no shim, shuffle: 0.07MB (mimalloc), 2.554s
+// shim, no shuffle: 6.05MB (mimalloc), 5.379s
+// shim, shuffle (5%, 1M obj): 62.05MB (mimalloc), 24.852s
+
 int main()
 {
-  //  Fragmenter<16, 16, 1000, 1, 100, true> frag;
-  Fragmenter<16, 16, 1000000, 500, 1000, true> frag;
-  for (auto i = 0; i < 4; i++) {
-    std::cout << malloc(16) << std::endl;
-  }
-  return 0;
+  constexpr int ObjectSize = 64;
+  constexpr int Iterations = 1000;
+  // Fragmenter<ObjectSize, ObjectSize, 1000000, 999, 1000, SHUFFLE> frag;
+  // Fragmenter<ObjectSize, ObjectSize, 1000000, 500, 1000, SHUFFLE> frag;
+  Fragmenter<ObjectSize, ObjectSize, 1000000, 100, 1000, SHUFFLE> frag;
+  //Fragmenter<ObjectSize, ObjectSize, 1000000, 50, 1000, SHUFFLE> frag;
+  std::cout << "starting " << getpid() << std::endl;
+
+#if USE_BUFFER
+  std::cout << "  using BufferManager ";
+#elif USE_SHIM
+  std::cout << "  using ShimBufferManager ";
+#endif
+
+#if SHUFFLE
+  std::cout << "(shuffled) ";
+#elif USE_SHIM
+#endif
+  
+  std::cout << std::endl;
+  
   using namespace BloombergLP;
-  bdlma::ShimBufferManager mgr;
-  for (auto it = 0; it < 100000; it++) {
-    for (auto i = 0; i < 1000; i++) {
-      volatile char * ch = (char *) mgr.allocate(16);
-      //      std::cout << "ch = " << (void *) ch << std::endl;
+  char * buf = new char[Iterations * ObjectSize];
+    for (auto it = 0; it < 10000000; it++) {
+  //for (auto it = 0; it < 1000000; it++) {
+#if USE_BUFFER
+    BloombergLP::bdlma::BufferManager mgr(buf, Iterations*ObjectSize);
+#elif USE_SHIM
+    bdlma::ShimBufferManager mgr;
+#endif
+    for (auto i = 0; i < Iterations; i++) {
+      volatile char * ch = (char *) mgr.allocate(ObjectSize);
+      memset((void *) ch, 13, ObjectSize);
+      volatile char buf[ObjectSize];
+      memcpy((void *) buf, (void *) ch, ObjectSize);
     }
-    mgr.reset();
   }
+  delete [] buf;
   return 0;
 }
