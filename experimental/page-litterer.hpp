93a6aa9ba41b7a514aee193158c2bac2836bcc7b
Nicolas van Kempen <nvankemp@gmail.com>
2021-06-29 10:02:03 -0400

    Add PageLitterer
    The idea is to write a litterer which will caust the worst possible benchmark.

---
+++ experimental/page-litterer.hpp
@@ -0,0 +1,114 @@
+#pragma once
+
+#ifndef PAGE_LITTERER_HPP
+#define PAGE_LITTERER_HPP
+
+#include <algorithm> // std::sort
+#include <cassert>
+#include <malloc.h> // malloc_usable_size
+#include <random>
+#include <set>
+
+long abs(long l) { return (l < 0L) ? -l : l; }
+
+/***
+ * The intuition behind this first version is to keep allocating until we reach PageSize bytes, forming a group of at
+ * least that size. Repeat to obtain NPages groups of at least PageSize bytes. Then, free a single object from each.
+ ***/
+class PageLittererV1 {
+public:
+  PageLittererV1(int MinSize, int MaxSize, int NPages, size_t Seed, long PageSize = sysconf(_SC_PAGESIZE)) {
+    std::vector<std::vector<void*>> allocated(NPages);
+    std::default_random_engine gen(Seed);
+    std::uniform_int_distribution dist(MinSize, MaxSize);
+
+    int pagesFilled = 0;
+    long currentPageFill = 0;
+
+    while (pagesFilled != NPages) {
+      size_t size = dist(gen);
+      auto ptr = ::malloc(size);
+      allocated[pagesFilled].push_back(ptr);
+
+      currentPageFill += ::malloc_usable_size(ptr);
+      if (currentPageFill >= PageSize) {
+        ++pagesFilled;
+        currentPageFill = 0;
+      }
+    }
+
+    for (int i = 0; i < NPages; ++i) {
+      std::uniform_int_distribution<int> dist(0, allocated[i].size() - 1);
+      int index = dist(gen);
+      free(allocated[i][index]);
+    }
+  }
+};
+
+/***
+ * This second method allocates objects, then sorts the addresses, and proceeds to free an object at least every
+ * PageSize bytes. Some corner cases need to be handled, mainly because it is hard to know how many objects we will
+ * actually need to allocate to be able to partition enough pages.
+ ***/
+class PageLittererV2 {
+public:
+  PageLittererV2(int MinSize, int MaxSize, int NPages, size_t Seed, long PageSize = sysconf(_SC_PAGESIZE)) {
+    std::vector<void*> allocated;
+    std::default_random_engine gen(Seed);
+    std::uniform_int_distribution dist(MinSize, MaxSize);
+
+    int AverageObjectSize = (MinSize + MaxSize) / 2;
+    int NAllocations = guess(AverageObjectSize, 0, 0, NPages, PageSize);
+    int PagesFilled = 0;
+
+    while (PagesFilled < NPages) {
+      allocated.reserve(NAllocations);
+
+      for (int i = 0; i < NAllocations; ++i) {
+        size_t size = dist(gen);
+        auto ptr = ::malloc(size);
+        allocated.push_back(ptr);
+      }
+
+      // Recount how many pages our current allocations are filling.
+      std::sort(allocated.begin(), allocated.end());
+      PagesFilled = 0;
+      char* previous = (char*) allocated[0];
+      for (int i = 1; i < allocated.size(); ++i) {
+        if (abs((char*) allocated[i] - previous) >= PageSize) {
+          previous = (char*) allocated[i];
+          ++PagesFilled;
+        }
+      }
+
+      // If we fell short, allocate more.
+      if (PagesFilled < NPages) {
+        NAllocations = guess(AverageObjectSize, NAllocations, PagesFilled, NPages, PageSize);
+      }
+    }
+
+    // allocated is already sorted.
+
+    PagesFilled = 0;
+    char* previous = (char*) allocated[0];
+    for (int i = 1; i < allocated.size(); ++i) {
+      if (abs((char*) allocated[i] - previous) >= PageSize) {
+        ::free(previous);
+        previous = (char*) allocated[i];
+        ++PagesFilled;
+      }
+    }
+
+    assert(PagesFilled >= NPages);
+  }
+
+private:
+  // Estimate total allocations that would be needed to reach the goal.
+  // The first estimate seems to be overshooting in a majority of cases anyway because of alignment and metadata.
+  int guess(int AverageObjectSize, int AlreadyAllocated, int PagesFilled, int TargetPages, long PageSize) {
+    return (AlreadyAllocated && PagesFilled) ? (AlreadyAllocated * TargetPages) / PagesFilled
+                                             : (TargetPages * PageSize) / AverageObjectSize;
+  }
+};
+
+#endif
--- experimental/test-shim-buffermanager.cpp
+++ experimental/test-shim-buffermanager.cpp
@@ -1,27 +1,29 @@
-#include <unistd.h>
 #include <algorithm>
-#include <iostream>
-#include <random>
-#include <string.h>
 #include <bdlma_buffermanager.h>
-#include <string>
-#include <cstring>
-#include <memory>
 #include <chrono>
-#include <ratio>
+#include <cstring>
 #include <ctime>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <ratio>
+#include <string.h>
+#include <string>
+#include <unistd.h>
 
-#include "shim_buffermanager.hpp"
 #include "litterer.hpp"
+#include "page-litterer.hpp"
+#include "shim_buffermanager.hpp"
 
 #include "cxxopts.hpp"
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char* argv[]) {
   cxxopts::Options options(argv[0], "");
   options.add_options()
     ("help","Print command-line options")
     ("shuffle","Shuffle ('litter') randomly before starting")
+    ("page-litter-v1","(V1) Adversary case. If set, overrides all other litter options.")
+    ("page-litter-v2","(V2) Adversary case. If set, overrides all other litter options.")
     ("seed","Use this seed for shuffling", cxxopts::value<size_t>())
     ("buffer","Use the actual buffer implementation")
     ("shim","Use a shim buffer implementation")
@@ -39,7 +41,7 @@ int main(int argc, char * argv[])
     std::cout << options.help({""}) << std::endl;
     return 0;
   }
-  
+
   int ObjectSize = 256; // was 64
   if (result.count("object-size")) {
     ObjectSize = result["object-size"].as<int>();
@@ -50,7 +52,7 @@ int main(int argc, char * argv[])
     WorkingSet = result["working-set"].as<int>();
   }
   int Iterations = WorkingSet / ObjectSize;
-  
+
   int Loops = 10;
   if (result.count("loops")) {
     Loops = result["loops"].as<int>();
@@ -65,24 +67,23 @@ int main(int argc, char * argv[])
   if (result.count("litter-objects")) {
     litterObjects = result["litter-objects"].as<int>();
   }
-  
+
   using namespace std::chrono;
   high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
   size_t seed = 0;
-  if (result.count("shuffle") && result.count("seed")) {
+  if (result.count("seed")) {
     seed = result["seed"].as<size_t>();
+  } else {
+    std::random_device rd;
+    seed = rd();
   }
-  
-  // volatile Litterer frag (ObjectSize, ObjectSize, 10000000, 1, 10, result.count("shuffle"));
-  volatile Litterer frag (ObjectSize, ObjectSize, litterObjects, (int) (litterOccupancy * 100), 100, result.count("shuffle"), seed);
-  auto storedSeed = ((Litterer *) &frag)->getSeed();
- 
+
   int BytesToRead = ObjectSize;
   if (result.count("bytes-to-read")) {
     BytesToRead = result["bytes-to-read"].as<int>();
   }
-  
+
   if (result.count("buffer")) {
     std::cout << "using BufferManager ";
   } else {
@@ -94,62 +95,73 @@ int main(int argc, char * argv[])
     localityIterations = result["locality-iterations"].as<int>();
   }
 
-  if (result.count("shuffle")) {
-    std::cout << "(shuffled: seed = " << storedSeed << ") ";
+  if (result.count("page-litter-v2")) {
+    volatile PageLittererV2 frag(ObjectSize, ObjectSize, Iterations, seed);
+    std::cout << "(page-litter-v2: seed = " << seed << ")";
+  } else if (result.count("page-litter-v1")) {
+    volatile PageLittererV1 frag(ObjectSize, ObjectSize, Iterations, seed);
+    std::cout << "(page-litter-v1: seed = " << seed << ")";
+  } else {
+    volatile Litterer frag(ObjectSize, ObjectSize, litterObjects, (int) (litterOccupancy * 100), 100,
+                           result.count("shuffle"), seed);
+    if (result.count("shuffle")) {
+      std::cout << "(shuffled: seed = " << seed << ") ";
+    }
   }
+
   std::cout << std::endl;
   std::cout << "working set = " << WorkingSet << " bytes " << std::endl;
   std::cout << "object size = " << ObjectSize << std::endl;
   std::cout << "bytes to read = " << BytesToRead << std::endl;
   std::cout << "litter occupancy = " << litterOccupancy << std::endl;
-  std::cout << "loops = " << Loops <<  std::endl;
-  std::cout << "locality iterations = " << localityIterations <<  std::endl;
+  std::cout << "loops = " << Loops << std::endl;
+  std::cout << "locality iterations = " << localityIterations << std::endl;
 
   using namespace BloombergLP;
-  char * buf = new char[Iterations * ObjectSize];
-  auto ptrs = new volatile void * [Iterations];
-  BloombergLP::bdlma::BufferManager mgr_buffer(buf, Iterations*ObjectSize);
+  char* buf = new char[Iterations * ObjectSize];
+  auto ptrs = new volatile void*[Iterations];
+  BloombergLP::bdlma::BufferManager mgr_buffer(buf, Iterations * ObjectSize);
   bdlma::ShimBufferManager mgr_shim(nullptr, Iterations);
   auto which_buf = result.count("buffer");
   if (which_buf) {
-    std::cout << "buffer starts at " << (void *) buf << std::endl;
+    std::cout << "buffer starts at " << (void*) buf << std::endl;
   }
 
   // Start the actual benchmark.
-  
+
   high_resolution_clock::time_point t2 = high_resolution_clock::now();
   duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
 
   for (volatile auto it = 0; it < Loops; it++) {
-    
+
     for (volatile auto i = 0; i < Iterations; i++) {
-      volatile char * ch;
+      volatile char* ch;
       volatile int value = 13;
       if (which_buf) {
-	ch = (char *) mgr_buffer.allocate(ObjectSize);
+        ch = (char*) mgr_buffer.allocate(ObjectSize);
       } else {
-	ch = (char *) mgr_shim.allocate(ObjectSize);
-	if (i <= 1) {
-	  //	  std::cout << "object " << i << " = " << (void *) ch << std::endl;
-	}
+        ch = (char*) mgr_shim.allocate(ObjectSize);
+        if (i <= 1) {
+          // std::cout << "object " << i << " = " << (void*) ch << std::endl;
+        }
       }
-      memset((void *) ch, value, ObjectSize);
-      ptrs[i] = (void *) ch;
+      memset((void*) ch, value, ObjectSize);
+      ptrs[i] = (void*) ch;
     }
     volatile char ch;
     for (volatile auto it = 0; it < localityIterations; it++) {
       for (volatile auto i = 0; i < Iterations; i++) {
-	volatile char bufx[ObjectSize];
-	memcpy((void *) bufx, (void *) ptrs[i], BytesToRead);
-	ch += bufx[ObjectSize-1];
+        volatile char bufx[ObjectSize];
+        memcpy((void*) bufx, (void*) ptrs[i], BytesToRead);
+        ch += bufx[ObjectSize - 1];
       }
     }
 
     mgr_buffer.release();
     mgr_shim.release();
   }
-  delete [] ptrs;
-  delete [] buf;
+  delete[] ptrs;
+  delete[] buf;
   high_resolution_clock::time_point t3 = high_resolution_clock::now();
   duration<double> time_span2 = duration_cast<duration<double>>(t3 - t1);
   std::cout << "Time elapsed = " << time_span2.count() - time_span.count() << std::endl;
