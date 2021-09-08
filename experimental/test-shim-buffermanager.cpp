06e1511ca64f35aa96f6c8428c79998df4841283
Nicolas van Kempen <nvankempen@gmail.com>
2021-09-08 16:08:54 -0400

    Add optional sleep parameter to perf easily

--- experimental/test-shim-buffermanager.cpp
+++ experimental/test-shim-buffermanager.cpp
@@ -19,21 +19,20 @@
 
 int main(int argc, char* argv[]) {
   cxxopts::Options options(argv[0], "");
-  options.add_options()
-    ("help","Print command-line options")
-    ("shuffle","Shuffle ('litter') randomly before starting")
-    ("page-litter-v1","(V1) Adversary case. If set, overrides all other litter options.")
-    ("page-litter-v2","(V2) Adversary case. If set, overrides all other litter options.")
-    ("seed","Use this seed for shuffling", cxxopts::value<size_t>())
-    ("buffer","Use the actual buffer implementation")
-    ("shim","Use a shim buffer implementation")
-    ("object-size","Size of objects to allocate", cxxopts::value<int>())
-    ("bytes-to-read", "Bytes to read of each object (in locality iterations).", cxxopts::value<int>())
-    ("working-set","Size of working set (in bytes)", cxxopts::value<int>())
-    ("loops","Number of loops", cxxopts::value<int>())
-    ("litter-objects","Number of objects to litter", cxxopts::value<int>())
-    ("litter-occupancy", "Occupancy after littering (between 0 and 1).", cxxopts::value<float>())
-    ("locality-iterations","Locality iterations (non-allocating)", cxxopts::value<int>());
+  options.add_options()("help",
+                        "Print command-line options")("shuffle",
+                                                      "Shuffle ('litter') randomly before starting") ("page-litter-v1", "(V1) Adversary case. If set, overrides all other litter options.") ("page-litter-v2",
+                                                                                                                                                                                             "(V2) Adversary case. If set, overrides all other litter options.") ("sleep-timer", "Add a sleep timer between littering and benchmark, which could be used with perf.") ("seed", "Use this seed for shuffling", cxxopts::value<size_t>()) ("buffer", "Use the actual buffer implementation") ("shim", "Use a shim buffer implementation") ("object-size", "Size of objects to allocate", cxxopts::value<int>()) ("bytes-to-read",
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               "Bytes to read of each object (in locality iterations).",
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               cxxopts::
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   value<int>()) ("working-set", "Size of working set (in bytes)", cxxopts::value<int>()) ("loops", "Number of loops", cxxopts::value<int>()) ("litter-objects",
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               "Number of objects to litter",
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               cxxopts::value<
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   int>()) ("litter-occupancy", "Occupancy after littering (between 0 and 1).", cxxopts::value<float>()) ("locality-iterations",
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          "Locality iterations (non-allocating)",
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          cxxopts::
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              value<
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  int>());
 
   auto result = options.parse(argc, argv);
 
@@ -84,13 +83,18 @@ int main(int argc, char* argv[]) {
     BytesToRead = result["bytes-to-read"].as<int>();
   }
 
-  #ifndef NODEBUG
+  int SleepTimer = 0;
+  if (result.count("sleep-timer")) {
+    SleepTimer = result["sleep-timer"].as<int>();
+  }
+
+#ifndef NODEBUG
   if (result.count("buffer")) {
     std::cout << "using BufferManager ";
   } else {
     std::cout << "using ShimBufferManager ";
   }
-  #endif
+#endif
 
   int localityIterations = 1000;
   if (result.count("locality-iterations")) {
@@ -98,33 +102,33 @@ int main(int argc, char* argv[]) {
   }
 
   if (result.count("page-litter-v2")) {
-    #ifndef NODEBUG
+#ifndef NODEBUG
     std::cout << "(page-litter-v2: seed = " << seed << ")" << std::endl;
-    #endif
-    
+#endif
+
     volatile PageLittererV2 frag(ObjectSize, ObjectSize, Iterations, seed);
   } else if (result.count("page-litter-v1")) {
-    #ifndef NODEBUG
+#ifndef NODEBUG
     std::cout << "(page-litter-v1: seed = " << seed << ")" << std::endl;
-    #endif
+#endif
 
     volatile PageLittererV1 frag(ObjectSize, ObjectSize, Iterations, seed);
   } else if (result.count("shuffle")) {
-    #ifndef NODEBUG
+#ifndef NODEBUG
     std::cout << "(shuffled: seed = " << seed << ") " << std::endl;
-    #endif
+#endif
     volatile Litterer frag(ObjectSize, ObjectSize, litterObjects, (int) (litterOccupancy * 100), 100, seed);
   } else {
-    #ifndef NODEBUG
+#ifndef NODEBUG
     std::cout << std::endl;
-    #endif
+#endif
   }
 
-  #ifdef TRACE_MALLOC
+#ifdef TRACE_MALLOC
   _ = malloc(HMT_CHECKPOINT);
-  #endif
+#endif
 
-  #ifndef NODEBUG
+#ifndef NODEBUG
   std::cout << "working set = " << WorkingSet << " bytes " << std::endl;
   std::cout << "object size = " << ObjectSize << std::endl;
   std::cout << "bytes to read = " << BytesToRead << std::endl;
@@ -132,7 +136,7 @@ int main(int argc, char* argv[]) {
   std::cout << "loops = " << Loops << std::endl;
   std::cout << "locality iterations = " << localityIterations << std::endl;
   std::cout << "iterations = " << Iterations << std::endl;
-  #endif
+#endif
 
   using namespace BloombergLP;
   char* buf = new char[Iterations * ObjectSize];
@@ -141,13 +145,20 @@ int main(int argc, char* argv[]) {
   bdlma::ShimBufferManager mgr_shim(nullptr, Iterations);
   auto which_buf = result.count("buffer");
 
-  #ifndef NODEBUG
+#ifndef NODEBUG
   if (which_buf) {
     std::cout << "buffer starts at " << (void*) buf << std::endl;
   }
-  #endif
+#endif
 
   // Start the actual benchmark.
+
+  if (SleepTimer) {
+    std::cout << "Sleeping for " << SleepTimer << " seconds before starting benchmark (PID: " << getpid() << ")"
+              << std::endl;
+    sleep(SleepTimer);
+    std::cout << "Starting now."
+  }
 
   high_resolution_clock::time_point t2 = high_resolution_clock::now();
   duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
@@ -184,9 +195,9 @@ int main(int argc, char* argv[]) {
   delete[] buf;
   high_resolution_clock::time_point t3 = high_resolution_clock::now();
   duration<double> time_span2 = duration_cast<duration<double>>(t3 - t1);
-  #ifndef NODEBUG
+#ifndef NODEBUG
   std::cout << "Time elapsed = ";
-  #endif
+#endif
   std::cout << time_span2.count() - time_span.count() << std::endl;
   return 0;
 }
