ed27d08f868b02285d457f29c8ff7774c87b0e9c
Nicolas van Kempen <nvankemp@gmail.com>
2021-11-22 20:55:42 -0500

    Good progress, it looks like this should work.
    Need to add bin logic and final output but this should do the trick and
    be capable of outputting fequencies of reads / writes to size classes,
    with acceptable slowdown.

--- litter/pin-detector/src/detector.cpp
+++ litter/pin-detector/src/detector.cpp
@@ -4,121 +4,88 @@
 #include <iostream>
 #include <unordered_map>
 
-struct ThreadData {
-  ADDRINT Size;
-};
-
-struct CacheData {
-  size_t BinIndex;
-};
-
 static KNOB<std::string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "detector.out",
                                         "specify output file name");
 static std::ofstream OutputFile;
 
 static TLS_KEY TlsKey = INVALID_TLS_KEY;
-static std::unordered_map<ADDRINT, CacheData> Cache;
-static std::vector<unsigned long long int> Bins(sizeof(intptr_t) * 8);
 
-// NO_LOCKS is only (maybe) safe to enable in strictly single-threaded programs.
+// For performance benefits, we might want to reserve some space for this
+static std::unordered_map<ADDRINT, ADDRINT> addressToObjectSize;
+
+// This is only safe to set (maybe) on a strictly single-threaded program.
 #ifndef NO_LOCKS
 static PIN_RWMUTEX Lock;
-
-// Written for convenience. Easy to abuse so be careful.
-class PIN_RWMutexReadGuard {
-public:
-  PIN_RWMutexReadGuard(PIN_RWMUTEX* Lock) : _Lock(Lock) { PIN_RWMutexReadLock(_Lock); }
-
-  ~PIN_RWMutexReadGuard() { PIN_RWMutexUnlock(_Lock); }
-
-private:
-  PIN_RWMUTEX* _Lock;
-};
-
-class PIN_RWMutexWriteGuard {
-public:
-  PIN_RWMutexWriteGuard(PIN_RWMUTEX* Lock) : _Lock(Lock) { PIN_RWMutexWriteLock(_Lock); }
-
-  ~PIN_RWMutexWriteGuard() { PIN_RWMutexUnlock(_Lock); }
-
-private:
-  PIN_RWMUTEX* _Lock;
-};
 #endif
 
 VOID OnThreadStart(THREADID ThreadId, CONTEXT* Context, INT32 Flags, VOID* _) {
-  if (!PIN_SetThreadData(TlsKey, new ThreadData, ThreadId)) {
+  if (!PIN_SetThreadData(TlsKey, new ADDRINT, ThreadId)) {
     std::cerr << "Failed PIN_SetThreadData..." << std::endl;
     PIN_ExitProcess(1);
   }
 }
 
 VOID OnThreadEnd(THREADID ThreadId, const CONTEXT* Context, INT32 Code, VOID* _) {
-  delete static_cast<ThreadData*>(PIN_GetThreadData(TlsKey, ThreadId));
+  delete static_cast<ADDRINT*>(PIN_GetThreadData(TlsKey, ThreadId));
 }
 
 VOID OnMallocBefore(THREADID ThreadId, const CONTEXT* Context, ADDRINT Size) {
-  ThreadData* TData = static_cast<ThreadData*>(PIN_GetThreadData(TlsKey, ThreadId));
-  TData->Size = Size;
+  ADDRINT* SavedSizePointer = static_cast<ADDRINT*>(PIN_GetThreadData(TlsKey, ThreadId));
+  *SavedSizePointer = Size;
 }
 
 VOID OnMallocAfter(THREADID ThreadId, ADDRINT Pointer) {
-  ThreadData* TData = static_cast<ThreadData*>(PIN_GetThreadData(TlsKey, ThreadId));
+#ifndef NO_LOCKS
+  PIN_RWMutexWriteLock(&Lock);
+#endif
 
-  size_t BinIndex = 0;
-  while (BinIndex++ < Bins.size()) {
-    if (TData->Size >> BinIndex == 0) {
-      break;
-    }
+  ADDRINT SavedSize = *(static_cast<ADDRINT*>(PIN_GetThreadData(TlsKey, ThreadId)));
+  // std::cout << "malloc(" << SavedSize << ")" << std::endl;
+  for (ADDRINT i = Pointer; i < Pointer + SavedSize; ++i) {
+    addressToObjectSize[i] = SavedSize;
   }
 
-  CacheData CData{BinIndex};
-
 #ifndef NO_LOCKS
-  PIN_RWMutexWriteGuard _(&Lock);
+  PIN_RWMutexUnlock(&Lock);
 #endif
-  Cache[Pointer] = CData;
 }
 
 VOID OnFree(THREADID ThreadId, const CONTEXT* Context, ADDRINT Pointer) {
 #ifndef NO_LOCKS
-  PIN_RWMutexWriteGuard _(&Lock);
+  PIN_RWMutexWriteLock(&Lock);
 #endif
-  Cache.erase(Pointer);
+
+  if (addressToObjectSize.find(Pointer) != addressToObjectSize.end()) {
+    // std::cout << "free(" << Pointer << ")" << std::endl;
+    for (ADDRINT i = Pointer; i < Pointer + addressToObjectSize[Pointer]; ++i) {
+      addressToObjectSize.erase(i);
+    }
+  }
+
+#ifndef NO_LOCKS
+  PIN_RWMutexUnlock(&Lock);
+#endif
 }
 
 VOID OnMemoryRead(THREADID ThreadId, ADDRINT Pointer, UINT32 Size) {
 #ifndef NO_LOCKS
-  PIN_RWMutexReadGuard _(&Lock);
+  PIN_RWMutexReadLock(&Lock);
 #endif
 
-  if (Cache.find(Pointer) != Cache.end()) {
-    ++Bins[Cache[Pointer].BinIndex];
+  if (addressToObjectSize.find(Pointer) != addressToObjectSize.end()) {
+    // std::cout << "read(" << addressToObjectSize[Pointer] << ")" << std::endl;
   }
+
+#ifndef NO_LOCKS
+  PIN_RWMutexUnlock(&Lock);
+#endif
 }
 
 VOID OnMemoryWrite(THREADID ThreadId, ADDRINT Pointer, UINT32 Size) {
   // No statistics on writes yet...
 }
 
-VOID OnProgramEnd(INT32 Code, VOID* _) {
-#ifndef NO_LOCKS
-  PIN_RWMutexFini(&Lock);
-#endif
-
-  size_t MaxI = 0;
-  for (size_t i = 1; i < Bins.size(); ++i) {
-    if (Bins[i] > 0) {
-      MaxI = i;
-    }
-  }
-
-  OutputFile << "{ \"Bins\": [ " << Bins[0];
-  for (size_t i = 1; i <= MaxI; ++i) {
-    OutputFile << ", " << Bins[i];
-  }
-  OutputFile << "] }" << std::endl;
-}
+VOID OnProgramEnd(INT32 Code, VOID* _) {}
 
 VOID Instruction(INS Instruction, VOID* _) {
   if (INS_IsMemoryRead(Instruction) && !INS_IsStackRead(Instruction)) {
@@ -126,10 +93,11 @@ VOID Instruction(INS Instruction, VOID* _) {
                    IARG_MEMORYREAD_SIZE, IARG_END);
   }
 
-  if (INS_IsMemoryWrite(Instruction) && !INS_IsStackWrite(Instruction)) {
-    INS_InsertCall(Instruction, IPOINT_BEFORE, (AFUNPTR) OnMemoryWrite, IARG_THREAD_ID, IARG_MEMORYWRITE_EA,
-                   IARG_MEMORYWRITE_SIZE, IARG_END);
-  }
+  // // No statistics on writes yet...
+  // if (INS_IsMemoryWrite(Instruction) && !INS_IsStackWrite(Instruction)) {
+  //   INS_InsertCall(Instruction, IPOINT_BEFORE, (AFUNPTR) OnMemoryWrite, IARG_THREAD_ID, IARG_MEMORYWRITE_EA,
+  //                  IARG_MEMORYWRITE_SIZE, IARG_END);
+  // }
 }
 
 VOID Image(IMG Image, VOID* _) {
@@ -168,18 +136,18 @@ int main(int argc, char* argv[]) {
 
   OutputFile.open(KnobOutputFile.Value().c_str());
 
+  TlsKey = PIN_CreateThreadDataKey(nullptr);
+  if (TlsKey == INVALID_TLS_KEY) {
+    std::cerr << "Failed to initialize TLS key..." << std::endl;
+    PIN_ExitProcess(1);
+  }
+
 #ifndef NO_LOCKS
   if (!PIN_RWMutexInit(&Lock)) {
     std::cerr << "Failed to initialize lock..." << std::endl;
     PIN_ExitProcess(1);
   }
 #endif
-
-  TlsKey = PIN_CreateThreadDataKey(NULL);
-  if (TlsKey == INVALID_TLS_KEY) {
-    std::cerr << "Failed to initialize TLS key..." << std::endl;
-    PIN_ExitProcess(1);
-  }
 
   IMG_AddInstrumentFunction(Image, nullptr);
   INS_AddInstrumentFunction(Instruction, nullptr);
