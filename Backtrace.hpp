54bea4314b49161b41fbe6b7d4e69735e4622af8
Emery Berger <emery@C02CJKXXMD6M>
2022-01-02 22:31:30 -0500

    More useful traces on OS X (not tested for Linux compatibility).

--- Backtrace.hpp
+++ Backtrace.hpp
@@ -4,12 +4,12 @@
 #include <backtrace.h>
 #include <cxxabi.h>
 #include <stdlib.h>
+#include <dlfcn.h>
 
 #include <iostream>
 #include <string>
 #include <vector>
 
-// Small utility file I wrote while testing libbacktrace with Intel Pin.
 namespace Backtrace {
     namespace {
         static backtrace_state* state = nullptr;
@@ -20,7 +20,65 @@ namespace Backtrace {
         std::string filename;
         int lineno;
     };
+}
 
+extern "C" 
+int onStackFrame(void* data, uintptr_t pc, const char* filename, int lineno, const char* function) {
+  auto* backtrace = static_cast<std::vector<Backtrace::StackFrame>*>(data);
+
+	    Backtrace::StackFrame frame;
+
+            // If we have no information, look up info using the pc.
+            if (!function && !filename) {
+	      Dl_info info;
+	      dladdr((void *) pc, &info);
+	      int status;
+	      if (info.dli_sname) {
+		char* demangled_function = abi::__cxa_demangle(info.dli_sname, nullptr, nullptr, &status);
+		if (demangled_function) {
+		  frame.function = std::string(demangled_function);
+		} else {
+		  frame.function = std::string(info.dli_sname);
+		}
+	      }
+	      frame.filename = std::string(info.dli_fname);
+	      backtrace->insert(backtrace->begin(), frame);
+                return 0;
+            }
+
+	    // std::cerr << "in " << filename << " at line " << lineno << std::endl;
+
+            if (!function) {
+                frame.function = "[UNKNOWN]";
+            } else {
+                int status;
+                char* demangled_function = abi::__cxa_demangle(function, nullptr, nullptr, &status);
+		if (!demangled_function) {
+		  Dl_info info;
+		  int status = dladdr((void *) pc, &info);
+		  if (status) {
+		    filename = info.dli_sname;
+		  }
+		}
+                if (status == 0) {
+                    frame.function = std::string(demangled_function);
+                    static decltype(::free)* free = (decltype(::free)*) dlsym(RTLD_DEFAULT, "free");
+                    (*free)(demangled_function);
+                } else {
+                    frame.function = std::string(function);
+                }
+            }
+
+            frame.filename = (!filename) ? "[UNKNOWN]" : std::string(filename);
+            frame.lineno = lineno;
+
+            backtrace->insert(backtrace->begin(), frame);
+
+            return 0;
+        };
+
+// Small utility file I wrote while testing libbacktrace with Intel Pin.
+namespace Backtrace {
     void initialize(char* filename) {
         if (state == nullptr) {
             const auto onError = [](void*, const char* msg, int errnum) {
@@ -41,47 +99,6 @@ namespace Backtrace {
 	  std::cerr << "Error #" << errnum << " while getting backtrace. Message: " << msg << std::endl;
         };
 
-        const auto onStackFrame = [](void* data, uintptr_t pc, const char* filename, int lineno, const char* function) {
-            auto* backtrace = static_cast<std::vector<StackFrame>*>(data);
-
-            StackFrame frame;
-
-            // If we have no information, we skip this frame.
-            if (!function && !filename) {
-                return 0;
-            }
-
-            if (!function) {
-                frame.function = "[UNKNOWN]";
-            } else {
-                int status;
-                char* demangled_function = abi::__cxa_demangle(function, nullptr, nullptr, &status);
-		if (demangled_function) {
-		  // std::cerr << "DEMANGLED = " << demangled_function << std::endl;
-		} else {
-		  Dl_info info;
-		  int status = dladdr((void *) pc, &info);
-		  if (status) {
-		    filename = info.dli_sname;
-		    //		    std::cerr << "C FUNCTION NAME = " << info.dli_sname << std::endl;
-		  }
-		}
-                if (status == 0) {
-                    frame.function = std::string(demangled_function);
-                    static decltype(::free)* free = (decltype(::free)*) dlsym(RTLD_DEFAULT, "free");
-                    (*free)(demangled_function);
-                } else {
-                    frame.function = std::string(function);
-                }
-            }
-
-            frame.filename = (!filename) ? "[UNKNOWN]" : std::string(filename);
-            frame.lineno = lineno;
-
-            backtrace->insert(backtrace->begin(), frame);
-
-            return 0;
-        };
 
         std::vector<StackFrame> backtrace;
         backtrace_full(state, skip, onStackFrame, onError, &backtrace);
